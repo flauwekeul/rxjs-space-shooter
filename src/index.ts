60ca0bd2c52797061f214e17434e9f999aeccc06
Abbe Keultjes <flauwekeul@gmail.com>
2018-08-17 14:11:23 +0200

    add player

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { interval, range } from 'rxjs'
-import { map, mapTo, mergeMap, toArray } from 'rxjs/operators'
-import { SPEED, START_COUNT } from './settings'
+import { combineLatest, fromEvent, interval, range } from 'rxjs'
+import { map, mapTo, mergeMap, startWith, toArray } from 'rxjs/operators'
+import { PLAYER_Y_FROM_BOTTOM, SPEED, START_COUNT } from './settings'
 
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
@@ -9,7 +9,17 @@ document.body.appendChild(canvas)
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-const paintStars = (stars: Star[]) => {
+const drawTriangle = (x: number, y: number, width: number, color: string, direction: 'up' | 'down') => {
+    ctx.fillStyle = color
+    ctx.beginPath()
+    ctx.moveTo(x - width, y)
+    ctx.lineTo(x, direction === 'up' ? y - width : y + width)
+    ctx.lineTo(x + width, y)
+    ctx.lineTo(x - width, y)
+    ctx.fill()
+}
+
+const renderBackground = (stars: Star[]) => {
     ctx.fillStyle = '#000000'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
@@ -19,7 +29,16 @@ const paintStars = (stars: Star[]) => {
     })
 }
 
-const starStream = range(1, START_COUNT).pipe(
+const renderPlayer = ({ x, y }: Position) => {
+    drawTriangle(x, y, 20, '#0066ff', 'up')
+}
+
+const renderAll = ({ stars, playerPosition }: { stars: Star[], playerPosition: Position }) => {
+    renderBackground(stars)
+    renderPlayer(playerPosition)
+}
+
+const background = range(1, START_COUNT).pipe(
     map<number, Star>(() => ({
         x: Math.floor(Math.random() * canvas.width),
         y: Math.floor(Math.random() * canvas.height),
@@ -39,10 +58,33 @@ const starStream = range(1, START_COUNT).pipe(
             return star
         })),
     )),
-).subscribe(paintStars)
+)
 
-interface Star {
+const mouseMove = fromEvent<MouseEvent>(canvas, 'mousemove')
+
+const playerMove = mouseMove.pipe(
+    map<MouseEvent, Position>(event => ({
+        x: event.clientX,
+        y: canvas.height - PLAYER_Y_FROM_BOTTOM,
+    })),
+    startWith<Position>({
+        x: canvas.width / 2,
+        y: canvas.height - PLAYER_Y_FROM_BOTTOM,
+    }),
+)
+
+const game = combineLatest(
+    background,
+    playerMove,
+).pipe(
+    map(([stars, playerPosition]) => ({ stars, playerPosition })),
+).subscribe(renderAll)
+
+interface Position {
     x: number
     y: number
+}
+
+interface Star extends Position {
     size: number
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,3 @@
 export const SPEED = 40
 export const START_COUNT = 250
+export const PLAYER_Y_FROM_BOTTOM = 30
